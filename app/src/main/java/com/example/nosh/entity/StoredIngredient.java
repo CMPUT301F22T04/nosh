30ea356d8ab42d3f2c47922f1101fab6ff60fa09
Dekr0 <dekr0.dk@gmail.com>
2022-10-18 21:54:10 -0600

    Add Best Before Date field into StoredIngredient

--- app/src/main/java/com/example/nosh/entity/StoredIngredient.java
+++ app/src/main/java/com/example/nosh/entity/StoredIngredient.java
@@ -1,15 +1,26 @@
 package com.example.nosh.entity;
 
 
+import java.util.Date;
+
 public class StoredIngredient extends Ingredient {
 
+    private Date bestBeforeDate;
     private String location;
 
-    public StoredIngredient(int amount, int unit, String description,
+    public StoredIngredient(Date bestBeforeDate, int amount, int unit, String description,
                             String category, String location) {
         super(amount, unit, description, category);
-
+        this.bestBeforeDate = bestBeforeDate;
         this.location = location;
+    }
+
+    public Date getBestBeforeDate() {
+        return bestBeforeDate;
+    }
+
+    public void setBestBeforeDate(Date bestBeforeDate) {
+        this.bestBeforeDate = bestBeforeDate;
     }
 
     public String getLocation() {
