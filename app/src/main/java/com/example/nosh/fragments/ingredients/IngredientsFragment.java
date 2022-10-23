3a83bd92981e7092d5d65ab4bd9314bb6bb1bb7b
Dekr0 <dekr0.dk@gmail.com>
2022-10-23 13:40:40 -0600

    Inflates UI with RecyclerView and Add button; Added a inner listener classes to handle Event in Ingredient Storage UI.

--- app/src/main/java/com/example/nosh/fragments/ingredients/IngredientsFragment.java
+++ app/src/main/java/com/example/nosh/fragments/ingredients/IngredientsFragment.java
@@ -1,14 +1,25 @@
 package com.example.nosh.fragments.ingredients;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.startup.AppInitializer;
+
 import com.example.nosh.R;
+import com.example.nosh.controller.IngredientStorageController;
+import com.example.nosh.database.DatabaseAccessFactory;
+import com.example.nosh.database.DatabaseAccessFactoryInitializer;
+import com.example.nosh.database.IngredientStorageAccess;
+import com.example.nosh.entity.StoredIngredient;
+
+import java.util.ArrayList;
+import java.util.Date;
+
 
 /**
  * A simple {@link Fragment} subclass.
@@ -16,6 +27,32 @@ import com.example.nosh.R;
  * create an instance of this fragment.
  */
 public class IngredientsFragment extends Fragment {
+
+    private StoredIngredientAdapter adapter;
+
+    private IngredientStorageController controller;
+
+    private IngredientsFragmentListener listener;
+
+    private ArrayList<StoredIngredient> ingredients;
+
+    private class IngredientsFragmentListener implements
+            StoredIngredientAdapter.RecyclerViewListener, View.OnClickListener {
+
+        @Override
+        public void onClick(View v) {
+
+        }
+
+        @Override
+        public void onDeleteButtonClick(int pos) {
+            controller.remove(ingredients.get(pos));
+
+            ingredients.remove(pos);
+
+            adapter.notifyItemRemoved(pos);
+        }
+    }
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -51,15 +88,42 @@ public class IngredientsFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
+        DatabaseAccessFactory factory =
+                AppInitializer.getInstance(requireContext()).
+                        initializeComponent(DatabaseAccessFactoryInitializer.class);
+
+        controller = new IngredientStorageController(factory
+                .createAccessController(IngredientStorageAccess.class.getSimpleName()));
+        listener = new IngredientsFragmentListener();
+
+        controller.add(
+                new Date(), 1, 8, "STARBUCKS Iced Coffee",
+                "1.18 ~ 1.42 L", "Dairy", "Fridge");
+
+        controller.add(new Date(), 1, 4, "COCA-COLA Mini Cans",
+                "6x222 mL", "Drinks", "Fridge");
+
+        controller.add(new Date(), 1, 5, "KRAFT Peanut Butter",
+                "750g", "Pantry", "Pantry");
+
+        ingredients = controller.get();
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_ingredients, container, false);
+        View v = inflater.inflate(R.layout.fragment_ingredients, container, false);
+
+        RecyclerView recyclerView = v.findViewById(R.id.recycler_view);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getContext());
+
+        adapter = new StoredIngredientAdapter(listener, getContext(), ingredients);
+
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(adapter);
+
+        return v;
     }
 }
