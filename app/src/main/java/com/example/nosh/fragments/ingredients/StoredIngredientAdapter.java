fbbd13be15d451f09bab1c84ed40c0164864f500
Dekr0 <dekr0.dk@gmail.com>
2022-10-23 13:35:57 -0600

    Added Adapter and ViewHolder class for RecyclerView that display ingredient in the storage

---
+++ app/src/main/java/com/example/nosh/fragments/ingredients/StoredIngredientAdapter.java
@@ -0,0 +1,60 @@
+package com.example.nosh.fragments.ingredients;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.nosh.R;
+import com.example.nosh.entity.StoredIngredient;
+
+import java.util.ArrayList;
+
+
+public class StoredIngredientAdapter extends
+        RecyclerView.Adapter<StoredIngredientViewHolder> {
+
+    private final RecyclerViewListener listener;
+
+    private final Context context;
+
+    private final ArrayList<StoredIngredient> ingredients;
+
+    public StoredIngredientAdapter(RecyclerViewListener listener, Context context,
+                                   ArrayList<StoredIngredient> ingredients) {
+        this.listener = listener;
+        this.context = context;
+        this.ingredients = ingredients;
+    }
+
+    interface RecyclerViewListener {
+        void onDeleteButtonClick(int pos);
+    }
+
+    @NonNull
+    @Override
+    public StoredIngredientViewHolder onCreateViewHolder(@NonNull ViewGroup parent,
+                                                         int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.stored_ingredient_entry, parent, false);
+
+        return new StoredIngredientViewHolder(listener, view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull StoredIngredientViewHolder holder,
+                                 int position) {
+        holder.getNameTxtView().setText(ingredients.get(position).getName());
+        holder.getDescriptionTxtView().setText(ingredients.get(position).
+                getDescription());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return ingredients.size();
+    }
+}
