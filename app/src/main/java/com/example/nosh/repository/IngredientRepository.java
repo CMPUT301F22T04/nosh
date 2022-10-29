34ab129e6540ada9a8310f02137ce05d0b2a0a67
Dekr0 <dekr0.dk@gmail.com>
2022-10-29 15:07:16 -0600

    Implement update method; Slight change on signature of add and update method. Small refactor after adding Repository class

--- app/src/main/java/com/example/nosh/repository/IngredientRepository.java
+++ app/src/main/java/com/example/nosh/repository/IngredientRepository.java
@@ -1,16 +1,17 @@
 package com.example.nosh.repository;
 
-import com.example.nosh.database.DBController;
+import com.example.nosh.database.controller.DBController;
 import com.example.nosh.entity.Ingredient;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
+import java.util.Objects;
 import java.util.Observable;
-import java.util.Observer;
 
 
 /**
- * This class is the owner (SSOT) of all the Ingredient objects, which means 
+ * This class is the owner (SSOT) of all the Ingredient objects, which means
  * this class has the right modify or mutate those objects. The data exposed 
  * this class is immutable. In another word, classes that are other than 
  * this class do not have the ability to modify or mutate Ingredient objects.
@@ -19,45 +20,59 @@ import java.util.Observer;
  * so that the data in local is in sync with the database
  * 
  * This class also implements Observer Pattern. It's a subject because it needs to 
- * keep the UI layer up to date if there is any change in any of the Ingredient 
+ * keep the UI layer up to date if there is any change in any of the Ingredient
  * Object, and it's a observer because there's latency when fetching data from 
  * database. This class do not wait for data return after making a request.
  */
-public class IngredientRepository extends Observable implements Observer {
+public class IngredientRepository extends Repository {
 
-    private final DBController dbController;
     private final HashMap<String, Ingredient> ingredients;
 
     public IngredientRepository(DBController dbController) {
-        this.dbController = dbController;
-        ingredients = new HashMap<>();
+        super(dbController);
 
-        this.dbController.addObserver(this);
+        ingredients = new HashMap<>();
     }
 
-    public void add(Ingredient ingredient) {
-        ingredients.put(ingredient.getHashcode(), ingredient);
-        dbController.create(ingredient);
+    public void add(Date bestBeforeDate, double unit, int amount,
+                    String category, String description, String location,
+                    String name) {
 
-        notifyObservers();
+        Ingredient ingredient = new Ingredient(bestBeforeDate, unit, amount,
+                category, description, location, name);
+
+        ingredients.put(ingredient.getHashcode(), ingredient);
+
+        super.add(ingredient);
     }
 
     public ArrayList<Ingredient> retrieve() {
         return new ArrayList<>(ingredients.values());
     }
 
-    public void delete(Ingredient ingredient) {
-        ingredients.remove(ingredient.getHashcode());
-        dbController.delete(ingredient);
+    public void update(String hashcode, Date bestBeforeDate, double unit, int amount,
+                       String category, String description, String location,
+                       String name) {
+
+        Ingredient ingredient = Objects.requireNonNull(ingredients.get(hashcode));
+
+        ingredient.setBestBeforeDate(bestBeforeDate);
+        ingredient.setUnit(unit);
+        ingredient.setAmount(amount);
+        ingredient.setCategory(category);
+        ingredient.setDescription(description);
+        ingredient.setLocation(location);
+        ingredient.setName(name);
+
+        ingredients.put(hashcode, ingredient);
+
+        super.update(ingredient);
     }
 
-    /**
-     * Only call this method when first time instantiate an instance of this 
-     * class. It's for retrieving all data from database when app initially 
-     * start since there's no data locally.
-     */
-    public void sync() {
-        dbController.retrieve();
+    public void delete(Ingredient ingredient) {
+        ingredients.remove(ingredient.getHashcode());
+
+        super.delete(ingredient);
     }
 
     /**
@@ -70,16 +85,9 @@ public class IngredientRepository extends Observable implements Observer {
         Ingredient[] ingredients = (Ingredient[]) arg;
 
         for (Ingredient ingredient : ingredients) {
-            add(ingredient);
+            this.ingredients.put(ingredient.getHashcode(), ingredient);
         }
 
         notifyObservers();
-    }
-
-    @Override
-    public void notifyObservers() {
-        setChanged();
-
-        super.notifyObservers();
     }
 }
