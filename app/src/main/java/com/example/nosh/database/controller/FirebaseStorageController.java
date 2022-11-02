0e22cc6a10661b676459204669998c9902426ef9
Dekr0 <dekr0.dk@gmail.com>
2022-11-02 16:41:34 -0600

    Add FirebaseStorageController for controlling upload and download to Firebase Storage

---
+++ app/src/main/java/com/example/nosh/database/controller/FirebaseStorageController.java
@@ -0,0 +1,76 @@
+package com.example.nosh.database.controller;
+
+import android.content.Context;
+import android.net.Uri;
+import android.util.Log;
+
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+import javax.inject.Inject;
+
+
+public class FirebaseStorageController {
+
+    // Directory of a current user
+    private final StorageReference rootRef;
+    private final Context context;
+
+    public FirebaseStorageController(Context context, StorageReference ref) {
+        this.context = context;
+        this.rootRef = ref;
+        this.sync();
+    }
+
+    public void add(Uri image) {
+
+        StorageReference ref = rootRef.child(image.getLastPathSegment());
+
+        UploadTask uploadTask = ref.putFile(image);
+
+        uploadTask
+                .addOnSuccessListener(
+                taskSnapshot ->
+                        Log.d("Upload Image", "Upload successfully")
+        )
+                .addOnFailureListener(
+                        e ->
+                                Log.w("Upload Image", e)
+                );
+    }
+
+    /**
+     * Get all URLs of images in Firebase storage for the current user
+     */
+    private void sync() {
+        rootRef.listAll().addOnSuccessListener(listResult ->
+        {
+            try {
+                cacheIn(listResult.getItems());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+    }
+
+    private void cacheIn(List<StorageReference> refs) throws IOException {
+        for (StorageReference ref : refs) {
+            // Current file structure : /[uid]/...
+            String[] filename = ref.getPath().split("/")[2].split("\\.");
+            File tempFile = File.createTempFile(filename[0], filename[1]);
+
+            // Default location seems to be at DCIM folder
+            ref.getFile(tempFile)
+                    .addOnSuccessListener(
+                            taskSnapshot -> Log.d("Download", "Download Successfully")
+                    )
+                    .addOnFailureListener(
+                            e -> Log.w("Download", e)
+                    );
+        }
+    }
+}
