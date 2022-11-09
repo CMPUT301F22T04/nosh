3210e5242328dfaec03a2d92d627746f65766d43
Dekr0 <dekr0.dk@gmail.com>
2022-11-09 16:16:02 -0700

    Using Dagger 2 to build dependencies graph

--- app/src/main/java/com/example/nosh/injection/ApplicationComponent.java
+++ app/src/main/java/com/example/nosh/injection/ApplicationComponent.java
@@ -1,12 +1,37 @@
 package com.example.nosh.injection;
 
-import com.example.nosh.database.controller.FirebaseStorageController;
+import com.example.nosh.MainActivity;
+import com.example.nosh.auth.Login;
+import com.example.nosh.auth.Register;
+import com.example.nosh.fragments.ingredients.IngredientsFragment;
+import com.example.nosh.fragments.recipes.RecipesFragment;
+
+import javax.inject.Singleton;
 
 import dagger.Component;
 
 
-@Component
+// Definition of the Application dependencies graph
+@Singleton
+@Component(modules = FirebaseModule.class)
 public interface ApplicationComponent {
 
-    void inject(FirebaseStorageController storageController);
+    // Include application context (and Activity context)
+
+    // Tells Dagger that Login requests injection so that graph needs to
+    // satisfy all the dependencies of the fields that Login is injecting.
+    void inject(Login loginActivity);
+
+    // Tells Dagger that Register requests injection so that graph needs to
+    // satisfy all the dependencies of the fields that Register is injecting.
+    void inject(Register registerActivity);
+
+    // ...
+    void inject(MainActivity mainActivity);
+
+    // ...
+    void inject(IngredientsFragment ingredientsFragment);
+
+    // ...
+    void inject(RecipesFragment recipesFragment);
 }
