8d4dce0d1efc247abfb78c3c35bb6d9d01d74c07
Dekr0 <dekr0.dk@gmail.com>
2022-11-10 00:01:34 -0700

    Refactor Meal.java

--- app/src/main/java/com/example/nosh/entity/Meal.java
+++ app/src/main/java/com/example/nosh/entity/Meal.java
@@ -6,34 +6,39 @@ import com.google.firebase.Timestamp;
 import java.util.ArrayList;
 import java.util.Date;
 
+
 /**
  * This is the Meal class, each meals belong to a specific MealPlan day
  * Each Meal will have a name and a list of recipes and ingredients that make up that meal
  */
-public class Meal extends Foodstuff {
-    private final String hashcode; // id
-    private final ArrayList<Foodstuff> foodstuffs; // list of ingredients and recipes
+public class Meal {
+
+    private String name;
+
+    private final ArrayList<MealComponent> mealComponents; // list of ingredients and recipes
+
     private Boolean isExpanded;
 
+    private final String hashcode; // id
+
     public Meal(String name){
-        super(name);
+        this.name = name;
+
+        this.mealComponents = new ArrayList<>();
+
+        isExpanded = false;
+
         this.hashcode = Hashing.sha256().hashInt(new Timestamp(new Date()).getNanoseconds())
                 .toString();
-        this.foodstuffs = new ArrayList<>();
-        isExpanded = false;
     }
 
     // Getters and Setters
-    public ArrayList<Foodstuff> getFoodstuffs() {
-        return foodstuffs;
+    public ArrayList<MealComponent> getMealComponents() {
+        return mealComponents;
     }
 
-    public void addFoodstuff(Foodstuff foodstuff) {
-        this.foodstuffs.add(foodstuff);
-    }
-
-    public String getHashcode() {
-        return hashcode;
+    public void addMealComponent(MealComponent mealComponent) {
+        this.mealComponents.add(mealComponent);
     }
 
     public Boolean getExpanded() {
@@ -42,5 +47,17 @@ public class Meal extends Foodstuff {
 
     public void setExpanded(Boolean expanded) {
         isExpanded = expanded;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getHashcode() {
+        return hashcode;
     }
 }
