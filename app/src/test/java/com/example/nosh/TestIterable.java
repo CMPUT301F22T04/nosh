233cf58def395c45cfaf55094f73366c0ebf4128
Dekr0 <dekr0.dk@gmail.com>
2022-11-15 21:05:08 -0700

    Unit testing on Iterable and Deep copy interface

---
+++ app/src/test/java/com/example/nosh/TestIterable.java
@@ -0,0 +1,54 @@
+package com.example.nosh;
+
+import com.example.nosh.entity.Meal;
+import com.example.nosh.entity.MealComponent;
+import com.example.nosh.entity.MealPlanComponent;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.HashSet;
+
+
+public class TestIterable {
+
+    @Test
+    public void testMealIterable() {
+        MockIngredient ingredient = new MockIngredient();
+        MockRecipe recipe = new MockRecipe();
+
+        Meal meal = new Meal();
+
+        HashMap<String, MealComponent> mealComponents = new HashMap<>();
+
+        mealComponents.put(ingredient.getHashcode(), ingredient);
+        mealComponents.put(recipe.getHashcode(), recipe);
+
+        meal.setMealComponents(mealComponents);
+
+        for (MealComponent mealComponent :
+                meal) {
+            assert mealComponents.containsKey(mealComponent.getHashcode());
+        }
+    }
+
+    @Test
+    public void testMealPlanIterable() {
+        MockMeal mealA = new MockMeal();
+        MockMeal mealB = new MockMeal();
+
+        MealPlanComponent mealPlanComponent = new MealPlanComponent();
+
+        mealPlanComponent.addMeal(mealA);
+        mealPlanComponent.addMeal(mealB);
+
+        HashSet<String> hashSet = new HashSet<>();
+        hashSet.add(mealA.getHashcode());
+        hashSet.add(mealB.getHashcode());
+
+        for (Meal meal :
+                mealPlanComponent) {
+            assert hashSet.contains(meal.getHashcode());
+        }
+    }
+}
