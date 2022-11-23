5a641bc7c9ea43426557dce347ad0fe68c8e878c
Dekr0 <54260982+Dekr0@users.noreply.github.com>
2022-11-22 20:40:48 -0700

    Update ListFragment.java

--- app/src/main/java/com/example/nosh/fragments/list/ListFragment.java
+++ app/src/main/java/com/example/nosh/fragments/list/ListFragment.java
@@ -35,28 +35,20 @@ public class ListFragment extends Fragment {
     private ShoppingAdapter adapter;
     private IngredientRepository x;
     // IngredientsFragment depends on controller. Use Dagger to manager dependency injection
+    
     @Inject
     IngredientStorageController controller;
 
-
-    private com.example.nosh.fragments.Shopping.ShoppingFragment.ShoppingFragmentListener listener;
+    private ShoppingFragmentListener listener;
     private ArrayList<Ingredient> ingredients;
 
     private Button sortButton;
     private Object ShoppingFragment;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
     public ListFragment() {
         // Required empty public constructor
     }
+    
     private class ShoppingFragmentListener
             implements ShoppingAdapter.RecyclerViewListener,
             View.OnClickListener, FragmentResultListener {
@@ -73,52 +65,66 @@ public class ListFragment extends Fragment {
         }
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ListFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ListFragment newInstance(String param1, String param2) {
-        ListFragment fragment = new ListFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+    @Override
+    public void onAttach(@NonNull Context context) {
+
+        // Inject an instance of IngredientStorageController into IngredientFragment
+        ;
+        ((Nosh) context.getApplicationContext())
+                .getAppComponent()
+                .inject(this);
+
+        controller.addObserver(this);
+        
+        super.onAttach(context);
+        
+        listener = new ShoppingFragmentListener();
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
+        ingredients = controller.retrieve();
+
+        System.out.println(ingredients.get(1).getDescription());
+    }
+    
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-
-        // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_list, container, false);
-        System.out.println(ingredients.get(1).getDescription());
+        
         RecyclerView recyclerView = v.findViewById(R.id.list_recycler_view);
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getContext());
-
-
-
-        recyclerView.setLayoutManager(layoutManager);
+        
         adapter = new ShoppingAdapter(listener, getContext(), ingredients);
-        //adapter.update(ingredients);
+        
+        recyclerView.setLayoutManager(layoutManager);
         recyclerView.setAdapter(adapter);
 
-
-
         return v;
+    }
+
+
+    /**
+     * Receive notification from Ingredient Repository that there are new changes in
+     * data / entity objects. Retrieve the latest copy of the data
+     * @param observable
+     * @param o
+     */
+    @Override
+    public void update(Observable observable, Object o) {
+        ingredients = controller.retrieve();
+
+        adapter.update(ingredients);
+        adapter.notifyItemRangeChanged(0, ingredients.size());
+    }
+
+    @Override
+    public void onDestroy() {
+        controller.deleteObserver(this);
+
+        super.onDestroy();
     }
 }
