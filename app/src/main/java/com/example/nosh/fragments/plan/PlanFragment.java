a7599a8944da1b8301b07ff79d3765f8dfd595bf
Dekr0 <dekr0.dk@gmail.com>
2022-11-22 19:07:27 -0700

    Managing dependency injection; Add a testing method for testing back-end part for Meal Plan

--- app/src/main/java/com/example/nosh/fragments/plan/PlanFragment.java
+++ app/src/main/java/com/example/nosh/fragments/plan/PlanFragment.java
@@ -1,8 +1,10 @@
 package com.example.nosh.fragments.plan;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -12,36 +14,66 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
+import com.example.nosh.Nosh;
 import com.example.nosh.R;
 import com.example.nosh.activities.NewMealPlanActivity;
+import com.example.nosh.controller.MealPlanController;
+import com.example.nosh.entity.Ingredient;
+import com.example.nosh.entity.Meal;
+import com.example.nosh.entity.MealComponent;
 import com.example.nosh.entity.MealPlan;
+import com.example.nosh.entity.MealPlanComponent;
+import com.example.nosh.entity.Recipe;
 import com.example.nosh.fragments.plan.RecyclerViews.MealPlanRecyclerViewInterface;
 import com.example.nosh.fragments.plan.RecyclerViews.MealPlanRecyclerViewAdapter;
+import com.example.nosh.utils.DateUtil;
 
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.Observable;
+import java.util.Observer;
+
+import javax.inject.Inject;
+
 
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link PlanFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class PlanFragment extends Fragment implements MealPlanRecyclerViewInterface {
+public class PlanFragment extends Fragment implements
+        MealPlanRecyclerViewInterface, Observer {
+
     ArrayList<MealPlan> mealPlans = new ArrayList<>();
+
+    @Inject
+    MealPlanController controller;
 
     public PlanFragment() {
         // Required empty public constructor
     }
 
     public static PlanFragment newInstance(String param1, String param2) {
-        PlanFragment fragment = new PlanFragment();
-        return fragment;
+        return new PlanFragment();
+    }
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        ((Nosh) context.getApplicationContext())
+                .getAppComponent()
+                .inject(this);
+
+        controller.addObserver(this);
+
+        super.onAttach(context);
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+//        controller.add(generateTestedMealPlan());
     }
 
     @Override
@@ -64,6 +96,13 @@ public class PlanFragment extends Fragment implements MealPlanRecyclerViewInterf
         });
 
         return v;
+    }
+
+    @Override
+    public void onDestroy() {
+        controller.deleteObserver(this);
+
+        super.onDestroy();
     }
 
     private void setUpTestData(){
@@ -93,5 +132,47 @@ public class PlanFragment extends Fragment implements MealPlanRecyclerViewInterf
     private void launchNewMealPlanActivity() {
         Intent intent = new Intent(getContext(), NewMealPlanActivity.class);
         startActivity(intent);
+    }
+
+    private static MealPlan generateTestedMealPlan() {
+        MealPlan mealPlan = new MealPlan(
+                "Meal Plan 1",
+                DateUtil.getCalendar("2022-11-17").getTime(),
+                DateUtil.getCalendar("2022-11-27").getTime()
+        );
+
+        mealPlan.setHashcode("1");
+
+        Meal mealA = new Meal(5, "breakfast");
+        mealA.setHashcode("2");
+
+        Meal mealB = new Meal(10, "lunch");
+        mealB.setHashcode("3");
+
+        MealComponent ingredientA = new Ingredient();
+        ingredientA.setHashcode("4");
+        MealComponent recipeA = new Recipe();
+        recipeA.setHashcode("6");
+
+        MealComponent ingredientB = new Ingredient();
+        ingredientB.setHashcode("5");
+        MealComponent recipeB = new Recipe();
+        recipeB.setHashcode("7");
+
+        mealA.addMealComponent(ingredientA);
+        mealA.addMealComponent(recipeA);
+
+        mealB.addMealComponent(ingredientB);
+        mealB.addMealComponent(recipeB);
+
+        mealPlan.addMealToDay("2022-11-20", mealA);
+        mealPlan.addMealToDay("2022-11-25", mealB);
+
+        return mealPlan;
+    }
+
+    @Override
+    public void update(Observable o, Object arg) {
+
     }
 }
