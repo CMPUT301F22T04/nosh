702469d2d2b9370fdd93f1d783f32f12de454a7e
Dekr0 <dekr0.dk@gmail.com>
2022-11-22 19:03:49 -0700

    WIP Back-end part for MealPlan

--- app/src/main/java/com/example/nosh/database/controller/MealPlanDBController.java
+++ app/src/main/java/com/example/nosh/database/controller/MealPlanDBController.java
@@ -5,26 +5,77 @@ import android.util.Log;
 import com.example.nosh.entity.Hashable;
 import com.example.nosh.entity.Meal;
 import com.example.nosh.entity.MealPlan;
-import com.example.nosh.entity.MealPlanComponent;
 import com.example.nosh.utils.EntityUtil;
+import com.google.android.gms.tasks.Task;
+import com.google.android.gms.tasks.Tasks;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.Map;
+import java.util.Objects;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 import javax.inject.Inject;
+import javax.inject.Singleton;
 
 
+@Singleton
 public class MealPlanDBController extends DBController {
 
     static final String DOC_NAME = "meal_plan_storage";
     static final String COLLECTION_NAME = "meal_plans";
-    
+    static final String MEAL_PLAN_COMPONENTS = "meal_plan_components";
+    static final String MEALS = "meals";
+
+    private final ExecutorService executorService;
+
+    private class RetrieveMealTask implements Callable<Meal[]> {
+        private String hashcode;
+        private String date;
+
+        public RetrieveMealTask(String hashcode, String date) {
+            this.hashcode = hashcode;
+            this.date = date;
+        }
+
+        @Override
+        public Meal[] call() throws Exception {
+            CollectionReference mealCollection = ref
+                    .document(hashcode)
+                    .collection(MEAL_PLAN_COMPONENTS)
+                    .document(date)
+                    .collection(MEALS);
+
+            Task<QuerySnapshot> task = mealCollection.get();
+
+            QuerySnapshot snapshots = Tasks.await(task);
+
+            if (task.isSuccessful()) {
+                if (!snapshots.isEmpty()) {
+                    Meal[] meals = new Meal[task.getResult().size()];
+
+                    int counter = 0;
+                    for (DocumentSnapshot doc : task.getResult()) {
+
+                    }
+                }
+            }
+
+            return null;
+        }
+    }
 
     @Inject
     MealPlanDBController(CollectionReference ref) {
         super(ref.document(DOC_NAME).collection(COLLECTION_NAME));
+
+        executorService = Executors.newCachedThreadPool();
     }
 
     @Override
@@ -32,60 +83,97 @@ public class MealPlanDBController extends DBController {
         assertType(o);
 
         MealPlan mealPlan = (MealPlan) o;
+
         Map<String, Object> mealPlanData = EntityUtil.mealPlanToMap(mealPlan);
 
         // Create basic information about a meal plan (start and end date, name)
-        DocumentReference mealPlanDoc = ref.document(o.getHashcode());
-        mealPlanDoc.update(mealPlanData);
+        DocumentReference mealPlanDoc = ref.document(mealPlan.getHashcode());
+        mealPlanDoc
+                .set(mealPlanData)
+                .addOnSuccessListener(
+                        unused -> Log.i("CREATE",
+                                "DocumentSnapshot written with ID: " +
+                                        mealPlan.getHashcode()
+                        )
+                )
+                .addOnFailureListener(
+                        e -> Log.w("CREATE", "Error adding document")
+                );
+
 
         // Create a sub collection to contain meal plan components for each day
-        CollectionReference mealPlanComponentsCol = mealPlanDoc
-                .collection("meal_plan_components");
-
-        for (Map.Entry<String, MealPlanComponent> entry : mealPlan) {
-            DocumentReference mealPlanComponentDoc = mealPlanComponentsCol
-                    .document(entry.getKey());
-
-            mealPlanComponentDoc.update("hashcode", entry.getValue().getHashcode());
-
-            CollectionReference mealsCol = mealPlanComponentDoc.collection("meals");
-
-            for (Meal meal : entry.getValue()) {
-                DocumentReference mealDoc = mealsCol.document(meal.getHashcode());
-
-                Map<String, Object> mealData = EntityUtil.mealToMap(meal);
-
-                mealDoc.update(mealData);
-            }
-        }
-    }
-
-    @Override
-    public Object retrieve(Hashable o) {
-        return null;
+//        CollectionReference mealPlanComponentsCol = mealPlanDoc
+//                .collection("meal_plan_components");
+//
+//        for (Map.Entry<String, MealPlanComponent> entry : mealPlan) {
+//            DocumentReference mealPlanComponentDoc = mealPlanComponentsCol
+//                    .document(entry.getKey());
+//
+//            CollectionReference mealsCol = mealPlanComponentDoc.collection("meals");
+//
+//            for (Meal meal : entry.getValue()) {
+//                DocumentReference mealDoc = mealsCol.document(meal.getHashcode());
+//
+//                Map<String, Object> mealData = EntityUtil.mealToMap(meal);
+//
+//                mealDoc
+//                        .set(mealData)
+//                        .addOnSuccessListener(
+//                                unused -> Log.i("CREATE",
+//                                        "DocumentSnapshot written with ID: " +
+//                                                meal.getHashcode()
+//                                )
+//                        )
+//                        .addOnFailureListener(
+//                                e -> Log.w("UPDATE", "Error updating document", e)
+//                        );
+//            }
+//        }
     }
 
     @Override
     public void retrieve() {
-        ref.get()
-                .addOnCompleteListener(task -> {
-                    if (task.isSuccessful()) {
-                        MealPlan[] mealPlans =
-                                new MealPlan[task.getResult().size()];
+        try {
+            Future<MealPlan[]> mealPlansFuture =
+                    executorService.submit(this::retrieveMealPlans);
 
-                        int i = 0;
-                        for (DocumentSnapshot doc :
-                                task.getResult()) {
-                            mealPlans[i] = doc.toObject(MealPlan.class);
-                            mealPlans[i++].setHashcode(doc.getId());
-                        }
-                        setChanged();
-                        notifyObservers(mealPlans);
-                    } else {
-                        Log.w("RETRIEVE", "Cached get failed: ",
-                                task.getException());
-                    }
-                });
+            MealPlan[] mealPlans = mealPlansFuture.get();
+
+            assert mealPlans != null;
+            for (MealPlan mealPlan : mealPlans) {
+                for (String date : mealPlan.getPlans().keySet()) {
+                    Future<Meal[]> mealsFuture = executorService
+                            .submit(new RetrieveMealTask(mealPlan.getHashcode(), date));
+
+                    Meal[] meals = mealsFuture.get();
+                }
+            }
+        } catch (ExecutionException | InterruptedException e) {
+            Log.w("RETRIEVE", e);
+        }
+    }
+
+    private MealPlan[] retrieveMealPlans() throws ExecutionException, InterruptedException {
+        Task<QuerySnapshot> task = ref.get();
+
+        QuerySnapshot snapshots = Tasks.await(task);
+
+        if (task.isSuccessful()) {
+            if (!snapshots.isEmpty()) {
+                MealPlan[] mealPlans = new MealPlan[task.getResult().size()];
+
+                int counter = 0;
+                for (DocumentSnapshot doc :
+                        task.getResult()) {
+                    mealPlans[counter++] = EntityUtil
+                            .mapToMealPlan(Objects.requireNonNull(doc.getData()));
+                }
+
+                return mealPlans;
+            }
+        }
+
+        return null;
     }
 
     @Override
@@ -102,9 +190,6 @@ public class MealPlanDBController extends DBController {
                                 mealPlan.getHashcode() + "successfully updated"))
                 .addOnFailureListener(e ->
                         Log.w("UPDATE", "Error updating document", e));
-        /**
-         * Update foodStuffs will be added later
-         */
     }
 
     @Override
--- app/src/main/java/com/example/nosh/repository/MealPlanRepository.java
+++ app/src/main/java/com/example/nosh/repository/MealPlanRepository.java
@@ -1,27 +1,34 @@
 package com.example.nosh.repository;
 
-import com.example.nosh.database.controller.DBController;
+import com.example.nosh.database.controller.MealPlanDBController;
 import com.example.nosh.entity.MealPlan;
-import com.example.nosh.entity.MealPlanComponent;
+import com.example.nosh.entity.Transaction;
 
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.Objects;
 import java.util.Observable;
 
 import javax.inject.Inject;
+import javax.inject.Singleton;
 
+
+@Singleton
 public class MealPlanRepository extends Repository {
+
+    public static final String CREATE_NEW_MEAL_PLAN = "create_new_meal_plan";
+    public static final String MEAL_PLAN_HASHCODE = "meal_plan_hashcode";
 
     private final HashMap<String, MealPlan> mealPlans;
 
     @Inject
-    public MealPlanRepository(DBController dbController) {
+    public MealPlanRepository(MealPlanDBController dbController) {
         super(dbController);
 
         mealPlans = new HashMap<>();
 
-        sync();
+//        sync();
     }
 
     public void add(String name, Date startDate, Date endDate) {
@@ -31,7 +38,23 @@ public class MealPlanRepository extends Repository {
         MealPlan mealPlan = new MealPlan(name, startDate, endDate);
         mealPlans.put(mealPlan.getHashcode(), mealPlan);
 
+        dbController.create(mealPlan);
+
+        Transaction transaction = new Transaction(CREATE_NEW_MEAL_PLAN);
+
+        transaction.putContents(MEAL_PLAN_HASHCODE, mealPlan.getHashcode());
+
+        super.notifyObservers(transaction);
+    }
+
+    public void add(MealPlan mealPlan) {
+        mealPlans.put(mealPlan.getHashcode(), new MealPlan(mealPlan));
+
         super.add(mealPlan);
+    }
+
+    public MealPlan retrieve(String hashcode) {
+        return new MealPlan(Objects.requireNonNull(mealPlans.get(hashcode)));
     }
 
     public void update(MealPlan mealPlan) {
@@ -39,21 +62,23 @@ public class MealPlanRepository extends Repository {
     }
 
     public void delete(MealPlan mealPlan) {
+
     }
 
     public ArrayList<MealPlan> retrieve() {
         return new ArrayList<>(mealPlans.values());
     }
 
+    /**
+     * Retrieve all Meal Plan Documents
+     * Retrieve all Meal Documents for a single Meal Plan
+     * @param o
+     * @param arg
+     */
     @Override
     public void update(Observable o, Object arg) {
-        assert (arg instanceof MealPlan[]);
+        assert arg instanceof Transaction;
 
-        MealPlan[] mealPlans = (MealPlan[]) arg;
-        for (MealPlan mealPlan : mealPlans) {
-            this.mealPlans.put(mealPlan.getHashcode(), mealPlan);
-        }
-
-        notifyObservers();
+        Transaction transaction = (Transaction) arg;
     }
 }
